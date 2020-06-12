d54f594bad14865fc8fe9b2bcf1f4c160ef5958d
SteveBianco <steven.bianco@gmail.com>
2020-06-12 17:07:26 -0400

    Add get/set color methods to Canvas class

--- RayTraceChallenge/Canvas.cpp
+++ RayTraceChallenge/Canvas.cpp
@@ -1,8 +1,32 @@
 #include "Canvas.h"
 
+#include <stdexcept>
+
+
+unsigned int Canvas::toSingleIndex(unsigned int row, unsigned int col) const {
+	const auto idx = width_ * row + col;
+
+	if (idx >= pixels_.size()) {
+		throw std::out_of_range("Index out of bounds");
+	}
+
+	return idx;
+}
+
+
 Canvas::Canvas(unsigned int width, unsigned int height, Color color)
 	: width_(width)
 	, height_(height)
 	, pixels_(width * height, color) {
 
+}
+
+void Canvas::setPixel(unsigned int x, unsigned int y, const Color& color) {
+	const auto idx = toSingleIndex(x, y);
+	pixels_[idx] = color;
+}
+
+const Color& Canvas::getColor(unsigned int x, unsigned int y) const {
+	const auto idx = toSingleIndex(x, y);
+	return pixels_[idx];
 }
--- RayTraceChallenge/Canvas.h
+++ RayTraceChallenge/Canvas.h
@@ -9,6 +9,12 @@ class Canvas {
 	unsigned int width_, height_;
 	std::vector<Color> pixels_;
 
+
+	unsigned int toSingleIndex(unsigned int row, unsigned int col) const;
+
 public:
 	Canvas(unsigned int width, unsigned int height, Color fillColor = Color::White);
+
+	void setPixel(unsigned int x, unsigned int y, const Color& color);
+	const Color& getColor(unsigned int x, unsigned int y) const;
 };
